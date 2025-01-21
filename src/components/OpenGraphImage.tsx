9f305f3170a6f2e62f45c86f4c7c4db06b0c7f59
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-01-21 14:51:22 -0500

    add og image

---
+++ src/components/OpenGraphImage.tsx
@@ -0,0 +1,60 @@
+import type { JSX } from "react";
+export function OpenGraphImage({
+  sectionHead = "Frank's Book Log",
+  title,
+}: {
+  sectionHead?: string;
+  title: string;
+}): JSX.Element {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f4eedf",
+        display: "flex",
+        height: "630px",
+        position: "relative",
+        width: "1200px",
+      }}
+    >
+      <div
+        style={{
+          bottom: 0,
+          display: "flex",
+          flexDirection: "column",
+          paddingBottom: "64px",
+          paddingLeft: "80px",
+          paddingRight: "80px",
+          paddingTop: "32px",
+          position: "absolute",
+          width: "1200px",
+        }}
+      >
+        <div
+          style={{
+            color: "rgb(0 0 0 / 60%)",
+            fontFamily: "ArgentumSans",
+            marginBottom: "16px",
+            textTransform: "uppercase",
+          }}
+        >
+          {sectionHead}
+        </div>
+        <div
+          style={{
+            color: "rgb(0 0 0 / 75%)",
+            display: "flex",
+            flexWrap: "wrap",
+            fontFamily: "ArgentumSans",
+            fontSize: "88px",
+            fontWeight: 600,
+            lineHeight: 1,
+            textTransform: "uppercase",
+            textWrap: "balance",
+          }}
+        >
+          {title}
+        </div>
+      </div>
+    </div>
+  );
+}
