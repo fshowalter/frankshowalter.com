cd1022ba9ae7bad2e46cc80914be6f9b2851f7bf
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-02-14 11:55:38 -0500

    lint

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer(): JSX.Element {
   return (
     <div
       className={
-        "bg-footer px-container text-inverse tablet:px-12 tablet:pt-10 desktop:p-10 flex flex-wrap items-start justify-between gap-[10%] py-10"
+        "flex flex-wrap items-start justify-between gap-[10%] bg-footer px-container py-10 text-inverse tablet:px-12 tablet:pt-10 desktop:p-10"
       }
     ></div>
   );
