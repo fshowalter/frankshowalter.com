05c94432d312981a632f40f312beb37839b8f599
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-06-03 15:22:51 -0400

    update snaps

--- vitest.config.js
+++ vitest.config.js
@@ -8,9 +8,7 @@ export default getViteConfig({
       provider: "istanbul",
     },
     globals: true, // needed for testing-library teardown
-    // Vitest configuration options
-    setupFiles: ["setupTests.ts"],
-    workspace: [
+    projects: [
       {
         extends: true,
         test: {
@@ -20,5 +18,7 @@ export default getViteConfig({
         },
       },
     ],
+    // Vitest configuration options
+    setupFiles: ["setupTests.ts"],
   },
 });
