8977bd260fd2fab0895a6ce1b2a5b2127820fb26
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-09-03 22:42:41 -0400

    fix type

--- src/components/Home/ListItemValue.ts
+++ src/components/Home/ListItemValue.ts
@@ -2,10 +2,6 @@ import type { ImageProps } from "~/api/images";
 
 export type ListItemValue = {
   displayDate: string;
-  imageConfig: {
-    sizes: string;
-    width: number;
-  };
   imageProps: ImageProps;
   slug: string;
   stars: number;
