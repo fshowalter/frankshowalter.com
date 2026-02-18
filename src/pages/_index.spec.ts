1cb805516523ecedbeb5f4f49d9c5a28dd18c894
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-02-18 16:26:25 -0500

    wip

--- src/pages/_index.spec.ts
+++ src/pages/_index.spec.ts
@@ -3,51 +3,29 @@ import type { AstroComponentFactory } from "astro/runtime/server/index.js";
 import { getContainerRenderer as reactContainerRenderer } from "@astrojs/react";
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { loadRenderers } from "astro:container";
-import { promises as fs } from "node:fs";
-import path from "node:path";
-import { beforeEach, describe, it } from "vitest";
+import * as prettier from "prettier";
+import { describe, it } from "vitest";
+
+import { normalizeScriptSrc } from "~/utils/normalizeScriptSrc";
 
 import Index from "./index.astro";
 
 describe("/", () => {
-  let result: string;
-
-  beforeEach(async () => {
+  it("matches snapshot", { timeout: 40_000 }, async ({ expect }) => {
     const renderers = await loadRenderers([reactContainerRenderer()]);
     const container = await AstroContainer.create({ renderers });
-    result = await container.renderToString(Index as AstroComponentFactory, {
-      partial: false,
-      request: new Request(`https://www.frankshowalter.com/`),
-    });
-  });
-
-  it("includes movielog updates", async ({ expect }) => {
-    const movielogData = await fs.readFile(
-      path.join(__dirname, "../api/fixtures/data/movielog.json"),
-      "utf8",
+    const result = await container.renderToString(
+      Index as AstroComponentFactory,
+      {
+        partial: false,
+        request: new Request(`https://www.frankshowalter.com/`),
+      },
     );
 
-    const updates = JSON.parse(movielogData) as { title: string }[];
-
-    expect(updates.length).toBe(5);
-
-    for (const update of updates) {
-      expect(result).toContain(update.title.replaceAll("'", "&#x27;"));
-    }
-  });
-
-  it("includes booklog updates", async ({ expect }) => {
-    const movielogData = await fs.readFile(
-      path.join(__dirname, "../api/fixtures/data/booklog.json"),
-      "utf8",
-    );
-
-    const updates = JSON.parse(movielogData) as { title: string }[];
-
-    expect(updates.length).toBe(4);
-
-    for (const update of updates) {
-      expect(result).toContain(update.title.replaceAll("'", "&#x27;"));
-    }
+    await expect(
+      await prettier.format(normalizeScriptSrc(result), {
+        filepath: "index.html",
+      }),
+    ).toMatchFileSnapshot(`__snapshots__/index.html`);
   });
 });
